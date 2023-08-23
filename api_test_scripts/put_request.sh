668bad3ca0c62538bcf1048e31492945074bc361
markskeff <96910494+markskeff@users.noreply.github.com>
2023-08-23 21:43:14 +0100

    Update put_request.sh

--- api_test_scripts/put_request.sh
+++ api_test_scripts/put_request.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Sends a PUT request to a hypothetical endpoint using curl
-curl -X PUT -H "Content-Type: application/json" -d '{"name": "Updated Name"}' https://api.example.com/users/123
+# Sends a PUT request to an Insider endpoint using curl
+curl -X PUT -H "Content-Type: application/json" -d '{"name": "Updated Name"}' https://www.insider.com/
