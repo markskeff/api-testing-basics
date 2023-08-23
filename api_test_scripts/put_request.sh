6925a727ad4fded732d1f1980493bf522b7adf39
markskeff <96910494+markskeff@users.noreply.github.com>
2023-08-23 21:38:26 +0100

    Create put_request.sh

---
+++ api_test_scripts/put_request.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Sends a PUT request to a hypothetical endpoint using curl
+curl -X PUT -H "Content-Type: application/json" -d '{"name": "Updated Name"}' https://api.example.com/users/123
