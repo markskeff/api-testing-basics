cac68ae06f93f8ec5478df2a2edbdab60560103b
markskeff <96910494+markskeff@users.noreply.github.com>
2023-08-23 21:42:55 +0100

    Update post_request.sh

--- api_test_scripts/post_request.sh
+++ api_test_scripts/post_request.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Sends a POST request to a hypothetical API endpoint using curl
-curl -X POST -H "Content-Type: application/json" -d '{"name": "John Doe", "email": "john@example.com"}' https://api.example.com/users
+# Sends a POST request to an Insider endpoint using curl
+curl -X POST -H "Content-Type: application/json" -d '{"name": "John Doe", "email": "john@example.com"}' https://www.insider.com/
