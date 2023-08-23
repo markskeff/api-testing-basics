9fc95f5caa95204f0d8af8459dd4a26d0e6b6e0e
markskeff <96910494+markskeff@users.noreply.github.com>
2023-08-23 21:53:13 +0100

    Update put_request.sh

--- api_test_scripts/put_request.sh
+++ api_test_scripts/put_request.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Sending a PUT request using curl to update a book's information
-curl -X PUT -H "Content-Type: application/json" -d '{"title": "Updated Title", "author": "Jane Smith"}' https://api.example.com/books/123
+# Sending a PUT request using curl to update a post's information
+curl -X PUT -H "Content-Type: application/json" -d '{"title": "Updated Title", "body": "Updated body content", "userId": 1}' https://jsonplaceholder.typicode.com/posts/1
