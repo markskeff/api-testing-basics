52e044e2496665b08d241d2eb873ac5edf5d07ed
markskeff <96910494+markskeff@users.noreply.github.com>
2023-08-23 21:46:47 +0100

    Update post_request.sh

--- api_test_scripts/post_request.sh
+++ api_test_scripts/post_request.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# Sends a POST request to an Insider endpoint using curl
-curl -X POST -H "Content-Type: application/json" -d '{"name": "John Doe", "email": "john@example.com"}' https://www.insider.com/
+# Sending a POST request using curl to create a new book
+curl -X POST -H "Content-Type: application/json" -d '{"title": "New Book", "author": "John Doe"}' https://api.example.com/books
+
