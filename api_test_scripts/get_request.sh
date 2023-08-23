da045ab5081dddf4e39826c62eb1f481e0636b77
markskeff <96910494+markskeff@users.noreply.github.com>
2023-08-23 21:51:42 +0100

    Update get_request.sh

--- api_test_scripts/get_request.sh
+++ api_test_scripts/get_request.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-# Sending a GET request using curl to retrieve a list of books
-curl -X GET https://api.example.com/books
+# Sending a GET request using curl to retrieve a list of posts
+curl -X GET https://jsonplaceholder.typicode.com/posts
+
